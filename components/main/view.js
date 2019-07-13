c373d47d09cc6843684733d1a82deddf2aeadbc7
blake <blake.netzeband@gmail.com>
2019-07-13 08:30:54 -0600

    upgrade expo to 33

--- components/main/view.js
+++ components/main/view.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { ImageBackground } from "react-native";
 import PropTypes from "prop-types";
 import Amplify, { Storage } from "aws-amplify";
-import awsmobile from "./aws-exports";
+import awsmobile from "../../aws-exports";
 
 // style
 import mainStyles from "../../assets/styles/main";
@@ -15,43 +15,43 @@ import MainNextMeal from "./nextMeal";
 import MainChat from "./chat";
 
 class MainView extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			view: "nextMeal",
-			party: "Test Party"
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "nextMeal",
+      party: "Test Party",
+    };
 
-		// bind methods
-		this.handlePress = this.handlePress.bind(this);
+    // bind methods
+    this.handlePress = this.handlePress.bind(this);
 
-		// setup AWS storage
-		Amplify.configure(awsmobile);
-	}
+    // setup AWS storage
+    Amplify.configure(awsmobile);
+  }
 
-	handlePress(id) {
-		this.setState({ view: id });
-	}
+  handlePress(id) {
+    this.setState({ view: id });
+  }
 
-	render() {
-		return (
-			<ImageBackground
-				source={require("../../assets/images/octopus.png")}
-				style={mainStyles.imageBackground}
-			>
-				<MainTopBar party={this.state.party} />
+  render() {
+    return (
+      <ImageBackground
+        source={require("../../assets/images/octopus.png")}
+        style={mainStyles.imageBackground}
+      >
+        <MainTopBar party={this.state.party} />
 
-				{this.state.view == "profile" ? <MainProfile /> : null}
-				{this.state.view == "nextMeal" ? <MainNextMeal /> : null}
-				{this.state.view == "chat" ? <MainChat /> : null}
+        {this.state.view == "profile" ? <MainProfile /> : null}
+        {this.state.view == "nextMeal" ? <MainNextMeal /> : null}
+        {this.state.view == "chat" ? <MainChat /> : null}
 
-				<MainNavBar
-					selectedView={this.state.view}
-					handlePress={this.handlePress}
-				/>
-			</ImageBackground>
-		);
-	}
+        <MainNavBar
+          selectedView={this.state.view}
+          handlePress={this.handlePress}
+        />
+      </ImageBackground>
+    );
+  }
 }
 
 MainView.proptypes = {};
